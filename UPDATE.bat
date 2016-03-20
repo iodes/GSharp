734534c46cbc30af70ae28d5ed2d642271c843d1
SO HYUN SEOP <iodesme@gmail.com>
2016-03-21 01:17:06 +0900

    DLL 자동 복사 프로그램 추가

---
+++ UPDATE.bat
@@ -0,0 +1,33 @@
+@echo off
+COLOR 3E
+TITLE DLL UPDATE
+
+rem ---------------------- SETTINGS ----------------------
+set BIN=bin\Debug
+set GSHARP=%USERPROFILE%\Documents\SourceTree\gsharp
+set ENTRA_CORE=%USERPROFILE%\Documents\SourceTree\entra\DLL
+rem ---------------------- SETTINGS ----------------------
+
+echo 式式式式式式式式式式式式式式式式式式式式
+echo        _______   ____________  ___ 
+echo        / ____/ ^| / /_  __/__ ′/  ^|
+echo       / __/ /  ^|/ / / / / /_/ / /^| ^|
+echo      / /___/ /^|  / / / / _, _/ ___ ^|
+echo     /_____/_/ ^|_/ /_/ /_/ ^|_/_/  ^|_^|
+echo.
+echo 式式式式式式AUTO DLL UPDATER式式式式式式
+echo.
+
+if not exist %ENTRA_CORE% mkdir %ENTRA_CORE%
+copy %GSHARP%\GSharp.Base\%BIN%\GSharp.Base.dll %ENTRA_CORE%\GSharp.Base.dll
+copy %GSHARP%\GSharp.Compile\%BIN%\GSharp.Compile.dll %ENTRA_CORE%\GSharp.Compile.dll
+copy %GSHARP%\GSharp.Extension\%BIN%\GSharp.Extension.dll %ENTRA_CORE%\GSharp.Extension.dll
+copy %GSHARP%\GSharp.Graphic\%BIN%\GSharp.Graphic.dll %ENTRA_CORE%\GSharp.Graphic.dll
+copy %GSHARP%\GSharp.Manager\%BIN%\GSharp.Manager.dll %ENTRA_CORE%\GSharp.Manager.dll
+copy %GSHARP%\GSharp.Runtime\%BIN%\GSharp.Runtime.dll %ENTRA_CORE%\GSharp.Runtime.dll
+
+echo.
+echo            DLL UPDATE COMPLETE!
+echo.
+
+SET /P P=式式式式式式式式式式式式式式式式式式式式
