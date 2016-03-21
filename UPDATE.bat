36b77f2e681dba1719ea811eb00a8d1592170d23
SO HYUN SEOP <iodesme@gmail.com>
2016-03-21 20:00:49 +0900

    ENTRA Studio에 대응하도록 UPDATE.bat 파일을 수정

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,12 +1,13 @@
 @echo off
 COLOR 3E
 TITLE DLL UPDATE
-MODE con:cols=42 lines=25
+MODE con:cols=42 lines=30
 
 rem ---------------------- SETTINGS ----------------------
 set BIN=bin\Debug
 set GSHARP=%USERPROFILE%\Documents\SourceTree\gsharp
 set ENTRA_CORE=%USERPROFILE%\Documents\SourceTree\entra\DLL
+set ENTRA_STUDIO=%USERPROFILE%\Documents\SourceTree\entrastudio\DLL
 rem ---------------------- SETTINGS ----------------------
 
 echo ------------------------------------------
@@ -27,6 +28,14 @@ copy %GSHARP%\GSharp.Extension\%BIN%\GSharp.Extension.dll %ENTRA_CORE%\GSharp.Ex
 copy %GSHARP%\GSharp.Graphic\%BIN%\GSharp.Graphic.dll %ENTRA_CORE%\GSharp.Graphic.dll
 copy %GSHARP%\GSharp.Manager\%BIN%\GSharp.Manager.dll %ENTRA_CORE%\GSharp.Manager.dll
 copy %GSHARP%\GSharp.Runtime\%BIN%\GSharp.Runtime.dll %ENTRA_CORE%\GSharp.Runtime.dll
+
+if not exist %ENTRA_STUDIO% mkdir %ENTRA_STUDIO%
+copy %GSHARP%\GSharp.Base\%BIN%\GSharp.Base.dll %ENTRA_STUDIO%\GSharp.Base.dll
+copy %GSHARP%\GSharp.Compile\%BIN%\GSharp.Compile.dll %ENTRA_STUDIO%\GSharp.Compile.dll
+copy %GSHARP%\GSharp.Extension\%BIN%\GSharp.Extension.dll %ENTRA_STUDIO%\GSharp.Extension.dll
+copy %GSHARP%\GSharp.Graphic\%BIN%\GSharp.Graphic.dll %ENTRA_STUDIO%\GSharp.Graphic.dll
+copy %GSHARP%\GSharp.Manager\%BIN%\GSharp.Manager.dll %ENTRA_STUDIO%\GSharp.Manager.dll
+copy %GSHARP%\GSharp.Runtime\%BIN%\GSharp.Runtime.dll %ENTRA_STUDIO%\GSharp.Runtime.dll
 
 echo.
 echo            DLL UPDATE COMPLETE!
