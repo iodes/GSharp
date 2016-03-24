d396abfa22648b6c2c54353e0965f3f158b72f2a
Jo Tae Sang <gsts007@naver.com>
2016-03-25 06:55:36 +0900

    회원 블럭 추가

---
+++ GSharp.Graphic/Objects/MemberBlock.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GSharp.Graphic.Core;
+using GSharp.Graphic.Holes;
+using GSharp.Base.Cores;
+using GSharp.Base.Objects;
+
+namespace GSharp.Graphic.Objects
+{
+    /// <summary>
+    /// VariableBlock.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MemberBlock : ObjectBlock
+    {
+        // 생성자
+        public MemberBlock()
+        {
+            InitializeComponent();
+        }
+        
+        public GNumber GMember
+        {
+            get
+            {
+                return new GNumber(MemberCombo.SelectedIndex + 1);
+            }
+        }
+
+        public override GObject GObject
+        {
+            get
+            {
+                return GMember;
+            }
+        }
+
+        public override List<GBase> GObjectList
+        {
+            get
+            {
+                return new List<GBase> { GObject };
+            }
+        }
+    }
+}
