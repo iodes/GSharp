f549dd0ff09241072beac3cefcb1832d710d6cb5
Jo Tae Sang <gsts007@naver.com>
2016-04-06 17:27:59 +0900

    반복 블럭 추가
    * 무한 반복 블럭 추가
    * N번 블럭 추가

---
+++ GSharp.Graphic/Statements/LoopInfinityBlock.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GSharp.Graphic.Core;
+using GSharp.Graphic.Holes;
+using GSharp.Base.Statements;
+using GSharp.Base.Cores;
+using GSharp.Base.Logics;
+
+namespace GSharp.Graphic.Statements
+{
+    /// <summary>
+    /// IFBlock.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoopInfinityBlock : PrevStatementBlock
+    {
+        public LoopInfinityBlock()
+        {
+            InitializeComponent();
+        }
+
+        public GLoop GLoop
+        {
+            get
+            {
+                GLoop loop = new GLoop();
+
+                List<GBase> childList = ChildConnectHole?.StatementBlock?.GObjectList;
+                if (childList != null)
+                {
+                    foreach (GBase child in childList)
+                    {
+                        loop.Append((GStatement)child);
+                    }
+                }
+
+                return loop;
+            }
+        }
+
+        public override List<GBase> GObjectList
+        {
+            get
+            {
+                List<GBase> baseList = new List<GBase>() { GLoop };
+
+                List<GBase> nextList = NextConnectHole?.StatementBlock?.GObjectList;
+                if (nextList != null)
+                {
+                    baseList.AddRange(nextList);
+                }
+
+                return baseList;
+            }
+        }
+
+        public override GStatement GStatement
+        {
+            get
+            {
+                return GLoop;
+            }
+        }
+
+        public override List<BaseHole> HoleList
+        {
+            get
+            {
+                return new List<BaseHole> { NextConnectHole, ChildConnectHole };
+            }
+        }
+
+        public override NextConnectHole NextConnectHole
+        {
+            get
+            {
+                return RealNextConnectHole;
+            }
+        }
+    }
+}
---
+++ GSharp.Graphic/Statements/LoopNBlock.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GSharp.Graphic.Core;
+using GSharp.Graphic.Holes;
+using GSharp.Base.Statements;
+using GSharp.Base.Cores;
+using GSharp.Base.Logics;
+
+namespace GSharp.Graphic.Statements
+{
+    /// <summary>
+    /// IFBlock.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoopNBlock : PrevStatementBlock
+    {
+        public LoopNBlock()
+        {
+            InitializeComponent();
+        }
+
+        public GLoop GLoop
+        {
+            get
+            {
+                GObject obj = ObjectHole?.ObjectBlock?.GObject;
+                if (obj == null)
+                {
+                    throw new ToObjectException("반복문 블럭이 완성되지 않았습니다.", this);
+                }
+
+                GLoop loop = new GLoop(obj);
+
+                List<GBase> childList = ChildConnectHole?.StatementBlock?.GObjectList;
+                if (childList != null)
+                {
+                    foreach (GBase child in childList)
+                    {
+                        loop.Append((GStatement)child);
+                    }
+                }
+
+                return loop;
+            }
+        }
+
+        public override List<GBase> GObjectList
+        {
+            get
+            {
+                List<GBase> baseList = new List<GBase>() { GLoop };
+
+                List<GBase> nextList = NextConnectHole?.StatementBlock?.GObjectList;
+                if (nextList != null)
+                {
+                    baseList.AddRange(nextList);
+                }
+
+                return baseList;
+            }
+        }
+
+        public override GStatement GStatement
+        {
+            get
+            {
+                return GLoop;
+            }
+        }
+
+        public override List<BaseHole> HoleList
+        {
+            get
+            {
+                return new List<BaseHole> { ObjectHole, NextConnectHole, ChildConnectHole };
+            }
+        }
+
+        public override NextConnectHole NextConnectHole
+        {
+            get
+            {
+                return RealNextConnectHole;
+            }
+        }
+    }
+}
