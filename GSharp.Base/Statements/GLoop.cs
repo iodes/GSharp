0669b062d3dece7c3abf9325504a4c59d92350c1
Jo Tae Sang <gsts007@naver.com>
2016-04-06 17:26:08 +0900

    GLoop 구현

--- GSharp.Base/Statements/GLoop.cs
+++ GSharp.Base/Statements/GLoop.cs
@@ -1,7 +1,60 @@
-﻿namespace GSharp.Base.Statements
-{
-    public class GLoop
-    {
+﻿using System;
+using GSharp.Base.Cores;
+using System.Collections.Generic;
+using System.Text;
+using GSharp.Base.Utilities;
 
+namespace GSharp.Base.Statements
+{
+    public class GLoop : GStatement
+    {
+        #region 속성
+        public GObject Object { get; set; }
+        #endregion
+
+        #region 객체
+        private List<GStatement> listStatement = new List<GStatement>();
+        #endregion
+
+        #region 생성자
+        public GLoop(){}
+
+        public GLoop(GObject obj)
+        {
+            Object = obj;
+        }
+        #endregion
+
+        #region 사용자 함수
+        public void Append(GStatement obj)
+        {
+            listStatement.Add(obj);
+        }
+        #endregion
+
+        public override string ToSource()
+        {
+            StringBuilder builderCode = new StringBuilder();
+
+            if (Object != null)
+            {
+                String varName = "i";
+
+                builderCode.AppendFormat("for (int {0} = 0; {0} < {1}; {0}++)\n{{\n", varName, Object.ToSource());
+            }
+            else
+            {
+                builderCode.AppendLine("while (true)\n{\n");
+            }
+
+            foreach (GStatement statement in listStatement)
+            {
+                builderCode.Append(ConvertAssistant.Indentation(statement.ToSource()));
+            };
+
+            builderCode.Append("};");
+
+            return builderCode.ToString();
+        }
     }
 }
