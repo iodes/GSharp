f25f5de3315ed0e9d9eff00f0f9b741c4419a223
Jo Tae Sang <gsts007@naver.com>
2016-04-13 02:51:28 +0900

    덧셈 블럭 추가

---
+++ GSharp.Graphic/Objects/PlusBlock.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GSharp.Graphic.Core;
+using GSharp.Graphic.Holes;
+using GSharp.Base.Cores;
+using GSharp.Base.Objects;
+
+namespace GSharp.Graphic.Objects
+{
+    /// <summary>
+    /// NumberBlock.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PlusBlock : ObjectBlock
+    {
+        public GCompute GCompute
+        {
+            get
+            {
+                GObject obj1 = ObjectHole1.ObjectBlock.GObject;
+                GObject obj2 = ObjectHole2.ObjectBlock.GObject;
+                return new GCompute(obj1, GCompute.OperatorType.PLUS, obj2);
+            }
+        }
+
+        public override GObject GObject
+        {
+            get
+            {
+                return GCompute;
+            }
+        }
+
+        public override List<GBase> GObjectList
+        {
+            get
+            {
+                return new List<GBase> { GObject };
+            }
+        }
+
+        public PlusBlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
