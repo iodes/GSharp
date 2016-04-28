cc6a6a6b11140ffb893c46bc70e039ff4760e133
Jo Tae Sang <gsts007@naver.com>
2016-04-28 16:37:13 +0900

    StringBlock 및 NumberBlock을 추상 클래스로 생성

---
+++ GSharp.Graphic/Objects/StringBlock.cs
@@ -0,0 +1,18 @@
+﻿using GSharp.Base.Cores;
+using GSharp.Graphic.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Graphic.Objects
+{
+    public abstract class StringBlock : ObjectBlock
+    {
+        public StringBlock()
+        {
+
+        }
+    }
+}
