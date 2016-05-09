9ed1ad46cea51b3c70943d6cb2c571af55ae9905
SO HYUN SEOP <iodesme@gmail.com>
2016-05-09 22:38:04 +0900

    반복문 블럭 디자인 수정

--- GSharp.Graphic/Holes/NextConnectHole.xaml.cs
+++ GSharp.Graphic/Holes/NextConnectHole.xaml.cs
@@ -2,7 +2,9 @@
 using GSharp.Graphic.Statements;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +22,39 @@ namespace GSharp.Graphic.Holes
     /// <summary>
     /// NextConnectHole.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class NextConnectHole : BaseStatementHole
+    public partial class NextConnectHole : BaseStatementHole, INotifyPropertyChanged
     {
+        public Brush BodyColor
+        {
+            get
+            {
+                return _BodyColor;
+            }
+            set
+            {
+                _BodyColor = value;
+                OnPropertyChanged();
+            }
+        }
+        private Brush _BodyColor = Brushes.White;
+
+        public Brush BorderColor
+        {
+            get
+            {
+                return _BorderColor;
+            }
+            set
+            {
+                _BorderColor = value;
+                OnPropertyChanged();
+            }
+        }
+        private Brush _BorderColor = Brushes.DarkOrange;
+
         public override event HoleEventArgs BlockAttached;
         public override event HoleEventArgs BlockDetached;
-
-        public Brush Fill { get; set;}
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public override BaseBlock AttachedBlock
         {
@@ -77,6 +106,7 @@ namespace GSharp.Graphic.Holes
         public NextConnectHole()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         // 연결된 블럭을 제거
@@ -91,6 +121,11 @@ namespace GSharp.Graphic.Holes
             BlockHole.Child = null;
 
             return block;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
--- GSharp.Graphic/Holes/PrevConnectHole.xaml.cs
+++ GSharp.Graphic/Holes/PrevConnectHole.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,47 @@ namespace GSharp.Graphic.Holes
     /// <summary>
     /// PrevConnectHole.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class PrevConnectHole
+    public partial class PrevConnectHole : INotifyPropertyChanged
     {
+        public Brush BodyColor
+        {
+            get
+            {
+                return _BodyColor;
+            }
+            set
+            {
+                _BodyColor = value;
+                OnPropertyChanged();
+            }
+        }
+        private Brush _BodyColor = Brushes.White;
+
+        public Brush BorderColor
+        {
+            get
+            {
+                return _BorderColor;
+            }
+            set
+            {
+                _BorderColor = value;
+                OnPropertyChanged();
+            }
+        }
+        private Brush _BorderColor = Brushes.DarkOrange;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public PrevConnectHole()
         {
             InitializeComponent();
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
