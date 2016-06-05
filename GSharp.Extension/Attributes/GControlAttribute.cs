62f91b46b77a1b394988cf0e2104bacf723e3882
SO HYUN SEOP <iodesme@gmail.com>
2016-06-06 03:19:39 +0900

    GControl 속성 추가

---
+++ GSharp.Extension/Attributes/GControlAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GSharp.Extension.Attributes
+{
+    [AttributeUsage(AttributeTargets.Event | AttributeTargets.Property, AllowMultiple = false)]
+    public class GControlAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public GControlAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
