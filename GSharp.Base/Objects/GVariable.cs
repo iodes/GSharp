ba1671c30ac5ec27362f44d2f4b2adbecf68823b
Jo Tae Sang <gsts007@naver.com>
2016-06-09 10:14:38 +0900

    GCustom, GLogic, GString, GNumber Variable 제거 및 GVariable 추가

---
+++ GSharp.Base/Objects/GVariable.cs
@@ -0,0 +1,31 @@
+﻿using GSharp.Base.Cores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Base.Objects
+{
+    public class GVariable : GObject, ISettable
+    {
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+        }
+        private string _Name;
+
+        public GVariable(string name)
+        {
+            _Name = name;
+        }
+
+        public override string ToSource()
+        {
+            return Name;
+        }
+    }
+}
