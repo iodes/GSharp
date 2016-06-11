f0833f1053663850dd4210cbbe27167afa34fa0f
Jo Tae Sang <gsts007@naver.com>
2016-06-11 12:56:25 +0900

    List관련 블럭 추가

---
+++ GSharp.Graphic/Objects/ListBlock.cs
@@ -0,0 +1,28 @@
+﻿using GSharp.Base.Cores;
+using GSharp.Base.Objects;
+using GSharp.Graphic.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Graphic.Objects
+{
+    public abstract class ListBlock : ObjectBlock
+    {
+        public override GObject GObject
+        {
+            get
+            {
+                return GList;
+            }
+        }
+
+        public abstract GList GList { get; }
+
+        public ListBlock()
+        {
+        }
+    }
+}
