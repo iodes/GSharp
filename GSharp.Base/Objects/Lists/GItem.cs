3b2698389fb9ee954e9aba7d0c0024e7b6a55d6a
Jo Tae Sang <gsts007@naver.com>
2016-06-11 12:42:09 +0900

    List Object 추가

---
+++ GSharp.Base/Objects/Lists/GItem.cs
@@ -0,0 +1,32 @@
+﻿using GSharp.Base.Cores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Base.Objects.Lists
+{
+    public class GItem : GObject
+    {
+        private GObject Target;
+        private int index;
+
+        public GItem(GObject target, int idx)
+        {
+            Target = target;
+            index = idx;
+        }
+
+        public override string ToSource()
+        {
+            string targetStr = Target.ToSource();
+            if (!(Target is GList))
+            {
+                targetStr += ".ToList()";
+            }
+
+            return string.Format("{0}[{1}]", targetStr, index);
+        }
+    }
+}
---
+++ GSharp.Base/Statements/GListAdd.cs
@@ -0,0 +1,33 @@
+﻿using GSharp.Base.Cores;
+using GSharp.Base.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSharp.Base.Statements
+{
+    public class GListAdd : GStatement
+    {
+        public GObject List { get; set; }
+        public GObject Item { get; set; }
+
+        public GListAdd(GObject list, GObject item)
+        {
+            List = list;
+            Item = item;
+        }
+
+        public override string ToSource()
+        {
+            string listStr = List.ToSource();
+            if (!(List is GList))
+            {
+                listStr += ".ToList()";
+            }
+
+            return string.Format("{0}.Add({1})", listStr, Item.ToSource());
+        }
+    }
+}
