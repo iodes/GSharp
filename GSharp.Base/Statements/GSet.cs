32b89ef05012d9562f463f2df15c7c3f1a6d3c75
Jo Tae Sang <gsts007@naver.com>
2016-06-16 13:50:50 +0900

    GSet이 NullPointerException이 나지 않도록 변경

--- GSharp.Base/Statements/GSet.cs
+++ GSharp.Base/Statements/GSet.cs
@@ -26,32 +26,32 @@ namespace GSharp.Base.Statements
         {
             var builder = new StringBuilder();
 
-            if (GSettableObject.SettableType == typeof(object))
+            if (GSettableObject?.SettableType == typeof(object))
             {
-                return string.Format("{0} = {1};\n", GSettableObject.ToSource(), Value.ToSource());
+                return string.Format("{0} = {1};\n", GSettableObject?.ToSource(), Value?.ToSource());
             }
 
-            else if (GSettableObject.SettableType == typeof(bool))
+            else if (GSettableObject?.SettableType == typeof(bool))
             {
-                return string.Format("{0} = {1}.ToBool();\n", GSettableObject.ToSource(), Value.ToSource());
+                return string.Format("{0} = {1}.ToBool();\n", GSettableObject?.ToSource(), Value?.ToSource());
             }
 
-            else if (GSettableObject.SettableType == typeof(string))
+            else if (GSettableObject?.SettableType == typeof(string))
             {
-                return string.Format("{0} = {1}.ToText();\n", GSettableObject.ToSource(), Value.ToSource());
+                return string.Format("{0} = {1}.ToText();\n", GSettableObject?.ToSource(), Value?.ToSource());
             }
 
-            else if (GSettableObject.SettableType == typeof(double))
+            else if (GSettableObject?.SettableType == typeof(double))
             {
-                return string.Format("{0} = {1}.ToNumber();\n", GSettableObject.ToSource(), Value.ToSource());
+                return string.Format("{0} = {1}.ToNumber();\n", GSettableObject?.ToSource(), Value?.ToSource());
             }
 
-            else if (GSharpUtils.IsNumberType(GSettableObject.SettableType))
+            else if (GSharpUtils.IsNumberType(GSettableObject?.SettableType))
             {
-                return string.Format("{0} = ({2}){1}.ToNumber();\n", GSettableObject.ToSource(), Value.ToSource(), GSettableObject.SettableType.FullName);
+                return string.Format("{0} = ({2}){1}.ToNumber();\n", GSettableObject?.ToSource(), Value?.ToSource(), GSettableObject?.SettableType.FullName);
             }
 
-            return string.Format("{0} = ({2}){1}.ToCustom<{2}>();\n", GSettableObject.ToSource(), Value.ToSource(), GSettableObject.SettableType.FullName);
+            return string.Format("{0} = ({2}){1}.ToCustom<{2}>();\n", GSettableObject?.ToSource(), Value?.ToSource(), GSettableObject?.SettableType.FullName);
         }
     }
 }
