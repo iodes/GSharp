6ad4e24bdf4fe0036286a35cd5b65b657380e31a
SO HYUN SEOP <iodesme@gmail.com>
2017-03-12 19:26:06 +0900

    샘플 프로그램을 그래픽 방식으로 변경

---
+++ GSharpSample/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using GSharp.Base.Objects.Numbers;
+using GSharp.Extension;
+using GSharp.Graphic.Blocks;
+using GSharp.Graphic.Objects.Lists;
+using GSharp.Graphic.Objects.Logics;
+using GSharp.Graphic.Objects.Numbers;
+using GSharp.Graphic.Objects.Strings;
+using GSharp.Graphic.Scopes;
+using GSharp.Graphic.Statements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GSharpSample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBlock.Items.Add(new EventBlock(new GCommand("this", "Loaded", "프로그램이 시작될 때", typeof(void), GCommand.CommandType.Event)));
+            listBlock.Items.Add(new EventBlock(new GCommand("this", "Closing", "프로그램이 종료될 때", typeof(void), GCommand.CommandType.Event)));
+
+            listBlock.Items.Add(new IfBlock());
+            listBlock.Items.Add(new IfElseBlock());
+            listBlock.Items.Add(new SetBlock());
+
+            listBlock.Items.Add(new GateBlock());
+            listBlock.Items.Add(new CompareBlock());
+
+            listBlock.Items.Add(new EmptyListBlock());
+            listBlock.Items.Add(new ItemBlock());
+            listBlock.Items.Add(new ListAddBlock());
+
+            listBlock.Items.Add(new ComputeBlock(GCompute.OperatorType.PLUS));
+            listBlock.Items.Add(new ComputeBlock(GCompute.OperatorType.MINUS));
+            listBlock.Items.Add(new ComputeBlock(GCompute.OperatorType.MULTIPLY));
+            listBlock.Items.Add(new ComputeBlock(GCompute.OperatorType.DIVISION));
+            listBlock.Items.Add(new ComputeBlock(GCompute.OperatorType.MODULO));
+
+            listBlock.Items.Add(new StringCatBlock());
+            listBlock.Items.Add(new LengthBlock());
+
+            listBlock.Items.Add(new LoopInfinityBlock());
+            listBlock.Items.Add(new LoopNBlock());
+
+            foreach (BaseBlock block in listBlock.Items)
+            {
+                block.Margin = new Thickness(0, 0, 0, 10);
+            }
+        }
+    }
+}
--- GSharpSample/Properties/AssemblyInfo.cs
+++ GSharpSample/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
@@ -10,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GSharpSample")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +21,25 @@ using System.Runtime.InteropServices;
 // 해당 형식에 대해 ComVisible 특성을 true로 설정하세요.
 [assembly: ComVisible(false)]
 
-// 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("730cda8b-9911-4cbc-95f8-5d62cc838692")]
+//지역화 가능 응용 프로그램 빌드를 시작하려면 
+//.csproj 파일에서 <PropertyGroup> 내에 <UICulture>CultureYouAreCodingWith</UICulture>를
+//설정하십시오. 예를 들어 소스 파일에서 영어(미국)를
+//사용하는 경우 <UICulture>를 en-US로 설정합니다. 그런 다음 아래
+//NeutralResourceLanguage 특성의 주석 처리를 제거합니다. 아래 줄의 "en-US"를 업데이트하여
+//프로젝트 파일의 UICulture 설정과 일치시킵니다.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //테마별 리소스 사전의 위치
+                                     //(페이지, 앱 또는 모든 테마별 리소스 사전에 
+                                     // 리소스가 없는 경우에 사용됨)
+    ResourceDictionaryLocation.SourceAssembly //제네릭 리소스 사전의 위치
+                                              //(페이지, 앱 또는 모든 테마별 리소스 사전에 
+                                              // 리소스가 없는 경우에 사용됨)
+)]
+
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
