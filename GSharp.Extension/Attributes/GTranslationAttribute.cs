ce09d533ccd96ef20615a641492889213c88c88c
SO HYUN SEOP <iodesme@gmail.com>
2017-06-12 01:19:12 +0900

    GTranslation 속성 추가

---
+++ GSharp.Extension/Attributes/GTranslationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GSharp.Extension.Attributes
+{
+    [AttributeUsage(AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Enum)]
+    public class GTranslationAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public GTranslationAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
