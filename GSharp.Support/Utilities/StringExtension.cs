1085b2b04eba3ad258217308601267f20b26870f
SO HYUN SEOP <iodesme@gmail.com>
2017-10-09 04:29:29 +0900

    확장 메소드 정리

--- GSharp.Support/Utilities/StringExtension.cs
+++ GSharp.Support/Utilities/StringExtension.cs
@@ -7,19 +7,9 @@
             return val1.ToLower() == val2.ToLower();
         }
 
-        public static bool StringEquals(this object val1, string val2)
-        {
-            return val1.ToString().ToLower() == val2.ToLower();
-        }
-
         public static bool StringContains(this string val1, string val2)
         {
             return val1.ToLower().Contains(val2.ToLower());
-        }
-
-        public static bool StringContains(this object val1, string val2)
-        {
-            return val1.ToString().ToLower().Contains(val2.ToLower());
         }
     }
 }
