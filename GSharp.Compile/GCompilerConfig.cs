abbc4887c27936b5d79910cd00becdb996a41858
SO HYUN SEOP <iodesme@gmail.com>
2017-10-29 21:55:08 +0900

    어셈블리 정보 변경 기능 구현

--- GSharp.Compile/GCompilerConfig.cs
+++ GSharp.Compile/GCompilerConfig.cs
@@ -2,6 +2,22 @@
 {
     public class GCompilerConfig
     {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Company { get; set; }
+
+        public string Product { get; set; }
+
+        public string Copyright { get; set; }
+
+        public string Trademark { get; set; }
+
+        public string Version { get; set; }
+
+        public string FileVersion { get; set; }
+
         public bool IsEmbedded { get; set; } = false;
 
         public bool IsCompressed { get; set; } = false;
