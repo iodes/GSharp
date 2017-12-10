bc211db8d01ec14330e2328e206cbbf578cc541b
SO HYUN SEOP <iodesme@gmail.com>
2017-12-11 01:53:43 +0900

    호환 모드 디자인 구현

--- GSharpSample/Pages/LayoutMac.xaml.cs
+++ GSharpSample/Pages/LayoutMac.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GSharpSample.Pages
 {
@@ -23,6 +12,26 @@ namespace GSharpSample.Pages
         public LayoutMac()
         {
             InitializeComponent();
+        }
+
+        private void BtnClose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void BtnMinimize_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnMaximize_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = Application.Current.MainWindow.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void BtnDebug_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            pageWorkspace.Compile();
         }
     }
 }
