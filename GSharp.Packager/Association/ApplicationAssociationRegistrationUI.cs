de8596c08b2a7110a14928034f092cbcde469393
SO HYUN SEOP <iodesme@gmail.com>
2018-06-16 03:30:59 +0900

    Association 인터페이스 작성

---
+++ GSharp.Packager/Association/ApplicationAssociationRegistrationUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GSharp.Packager.Association
+{
+    [ComImport]
+    [Guid("1968106d-f3b5-44cf-890e-116fcb9ecef1")]
+    public class ApplicationAssociationRegistrationUI
+    {
+
+    }
+}
---
+++ GSharp.Packager/Association/IApplicationAssociationRegistrationUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GSharp.Packager.Association
+{
+    [Guid("1f76a169-f994-40ac-8fc8-0959e8874710")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IApplicationAssociationRegistrationUI
+    {
+        [PreserveSig]
+        int LaunchAdvancedAssociationUI([MarshalAs(UnmanagedType.LPWStr)] string pszAppRegName);
+    }
+}
