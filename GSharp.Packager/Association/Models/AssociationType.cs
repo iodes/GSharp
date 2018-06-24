a459f92bf7f420868288718a4f9ca1fd677018b5
SO HYUN SEOP <iodesme@gmail.com>
2018-06-24 16:55:01 +0900

    ProgID 등록 시스템 구현

---
+++ GSharp.Packager/Association/Models/AssociationType.cs
@@ -0,0 +1,20 @@
+﻿namespace GSharp.Packager.Association
+{
+    public class AssociationType
+    {
+        /// <summary>
+        /// 파일 확장자를 가져오거나 설정합니다.
+        /// </summary>
+        public string Extension { get; set; }
+
+        /// <summary>
+        /// 정보에 표시될 정보를 가져오거나 설정합니다.
+        /// </summary>
+        public string InfoTip { get; set; }
+
+        /// <summary>
+        /// 앱의 확장자 아이콘을 가져오거나 설정합니다.
+        /// </summary>
+        public string Icon { get; set; }
+    }
+}
--- GSharp.Packager/Association/Utilities/AssociationUtility.cs
+++ GSharp.Packager/Association/Utilities/AssociationUtility.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
 
 namespace GSharp.Packager.Association.Utilities
 {
-    class AssociationUtility
+    public static class AssociationUtility
     {
+        #region 상수
+        private const string keyDefault = "";
+        private const string keyInfoTip = "InfoTip";
+        private const string keyIcon = "DefaultIcon";
+        #endregion
+
+        #region 내부 함수
+        private static void CreateProgID(string name, AssociationType type)
+        {
+            // 레지스트리 등록
+            var progKey = Registry.ClassesRoot.CreateSubKey(name);
+            progKey.SetValue(keyDefault, $"{name.Replace(".", " ")} File");
+            progKey.SetValue(keyInfoTip, type.InfoTip);
+
+            // 아이콘 설정
+            var iconKey = progKey.CreateSubKey(keyIcon);
+            iconKey.SetValue(keyDefault, type.Icon);
+        }
+        #endregion
+
+        public static void Register(AssociationTarget target)
+        {
+            // Prog ID 등록
+            foreach (var type in target.Associations)
+            {
+                var extName = type.Extension.Replace(".", "").ToUpper();
+                var keyName = $"{target.CompanyName}.{target.DisplayName}.{extName}";
+
+                CreateProgID(keyName, type);
+            }
+        }
+
+        public static bool Launch(string appName = null)
+        {
+            var app = new ApplicationAssociationRegistrationUI() as IApplicationAssociationRegistrationUI;
+            var hresult = app.LaunchAdvancedAssociationUI(appName);
+
+            return Marshal.GetExceptionForHR(hresult) == null;
+        }
     }
 }
