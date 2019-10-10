5e52f9ff22cccf17fee4350ed621518ad56e6fe4
Kevin <iodesme@gmail.com>
2019-10-10 14:13:17 +0900

    GSharpUtils 임시 마이그레이션

--- GSharp.Base/Utilities/GSharpUtils.cs
+++ GSharp.Base/Utilities/GSharpUtils.cs
@@ -1,33 +1,46 @@
 ﻿using GSharp.Base.Cores;
 using GSharp.Base.Objects;
-using GSharp.Base.Objects.Customs;
-using GSharp.Base.Objects.Logics;
-using GSharp.Base.Objects.Numbers;
-using GSharp.Base.Objects.Strings;
-using Microsoft.CSharp;
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GSharp.Common.Objects;
 
 namespace GSharp.Base.Utilities
 {
     public static class GSharpUtils
     {
-        private static readonly Type[] numberTypes = {
-            typeof(sbyte),
-            typeof(byte),
-            typeof(short),
-            typeof(ushort),
+        private static readonly Type[] NumberTypes =
+        {
             typeof(int),
             typeof(uint),
             typeof(long),
             typeof(ulong),
             typeof(float),
-            typeof(double)
+            typeof(double),
+            typeof(byte),
+            typeof(sbyte),
+            typeof(short),
+            typeof(ushort)
         };
+
+        public static string GetTypeName(Type t)
+        {
+            if (!t.IsGenericType) return t.Name;
+            if (t.DeclaringType != null && t.IsNested && t.DeclaringType.IsGenericType)
+                throw new NotImplementedException();
+
+            var txt = t.Name.Substring(0, t.Name.IndexOf('`')) + "<";
+            var cnt = 0;
+
+            foreach (var arg in t.GetGenericArguments())
+            {
+                if (cnt > 0) txt += ", ";
+                txt += GetTypeName(arg);
+                cnt++;
+            }
+
+            return txt + ">";
+        }
 
         public static GVariable CreateGVariable(string variableName)
         {
@@ -36,37 +49,30 @@ namespace GSharp.Base.Utilities
 
         public static string CastParameterString(GObject obj, Type type)
         {
-            var compiler = new CSharpCodeProvider();
-            var castType = new CodeTypeReference(type);
-            var castString = compiler.GetTypeOutput(castType);
-            
-            // 변환이 필요없음
             if (type == typeof(object))
-            {
                 return obj?.ToSource();
-            }
 
-            // 같은 타입임
             if (
-                obj is GNumber && type == typeof(double) ||
-                obj is GString && type == typeof(string) ||
-                obj is GLogic && type == typeof(bool) ||
-                obj is GList && type == typeof(List<object>) ||
-                obj is ICustom && type == (obj as ICustom).CustomType
+                obj is GNumber && type == typeof(double) || obj is GString && type == typeof(string) ||
+                obj is GLogic && type == typeof(bool) || obj is GList && type == typeof(List<object>) ||
+                obj is ICustomObject customObject && type == customObject.CustomType
             )
             {
-                return obj?.ToSource();
+                return obj.ToSource();
             }
 
-            // To Number
+            var castString = GetTypeName(type);
+
             if (IsNumberType(type))
             {
                 if (obj is GNumber)
                 {
-                    return $"({castString}){obj?.ToSource()}";
+                    return $"({castString}){obj.ToSource()}";
                 }
 
-                return type == typeof(double) ? $"{obj?.ToSource()}.ToNumber()" : $"({castString}){obj?.ToSource()}.ToNumber()";
+                return type == typeof(double)
+                    ? $"{obj?.ToSource()}.ToNumber()"
+                    : $"({castString}){obj?.ToSource()}.ToNumber()";
             }
 
             if (type == typeof(string))
@@ -79,43 +85,29 @@ namespace GSharp.Base.Utilities
                 return $"{obj?.ToSource()}.ToBool()";
             }
 
-            // is List
-            if (IsListType(type))
+            if (!IsListType(type))
+                return type == typeof(object) ? obj?.ToSource() : $"{obj?.ToSource()}.ToCustom<{castString}>()";
+
+            var listType = type.GetGenericArguments()[0];
+
+            if (obj is GList || obj is ICustomObject o && IsListType(o.CustomType))
             {
-                Type listType = type.GetGenericArguments()[0];
-
-                if (obj is GList || obj is ICustom && IsListType((obj as ICustom).CustomType))
-                {
-                    return $"{obj?.ToSource()}.Select(e => e.ToCustom<{listType.ToString()}>()).ToList()";
-                }
-                
-                if (listType == typeof(object))
-                {
-                    return $"{obj?.ToSource()}.ToList()";
-                }
-
-                var listCastType = new CodeTypeReference(listType);
-                string listCastString = compiler.GetTypeOutput(castType);
-
-                return $"{obj?.ToSource()}.ToList().Select(e => e.ToCustom<{listCastString}>()).ToList()";
+                return $"{obj.ToSource()}.Select(e => e.ToCustom<{listType}>()).ToList()";
             }
 
-            if (type == typeof(object))
-            {
-                return obj?.ToSource();
-            }
-
-            return $"{obj?.ToSource()}.ToCustom<{castString}>()";
+            return listType == typeof(object)
+                ? $"{obj?.ToSource()}.ToList()"
+                : $"{obj?.ToSource()}.ToList().Select(e => e.ToCustom<{castString}>()).ToList()";
         }
 
-        public static bool IsListType(Type type)
+        private static bool IsListType(Type type)
         {
             return type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(List<>));
         }
 
-        public static bool IsNumberType(Type type)
+        private static bool IsNumberType(Type type)
         {
-            return numberTypes.Contains(type);
+            return NumberTypes.Contains(type);
         }
     }
 }
